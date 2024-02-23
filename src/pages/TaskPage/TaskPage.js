8cbb492d1624cb1faf8c074652a30f42605f6dfe
Galina Grogoryan <gala.april.ca@gmail.com>
2024-02-22 23:17:50 -0500

    creating functionality

--- src/pages/TaskPage/TaskPage.js
+++ src/pages/TaskPage/TaskPage.js
@@ -1,15 +1,25 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+// const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 export default function TaskPage() {
   const location = useLocation();
-  const { date } = location.state || {}; // Assuming you're passing date as state
+  const { date } = location.state || {};
 
   return (
-    <div>
-      <h1>
-        Tasks for {date ? new Date(date).toDateString() : "Select a Date"}
-      </h1>
+    // <div>
+    //   <h1>
+    //     Tasks for {date ? new Date(date).toDateString() : "Select a Date"}
+    //   </h1>
+    // </div>
+    <div className="tasks">
+      <div className="wrapper">
+        <input type="text" placeholder="Enter..." />
+        <button className="Enter"></button>
+      </div>
     </div>
   );
 }
